07579970469c72697cc7ad38cbdf25a4ab5702ae
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2022-02-22 17:42:05 -0600

    ComuEntreCom-Props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,11 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+// import React, { Fragment } from 'react';
 
 const   PrimeraApp = () => {
+
+    const saludo = "Hola Mundo ";
 
 
     return (
         <>
-            <h1>Hola Mundo</h1>;
+            <h1>{ saludo }</h1>;
+            {/* <pre> { JSON.stringify( saludo, null, 3) }</pre> */}
             <p>Mi primer aplicación</p>
         </>
     );

bb55594faeb6457d66875b20e57c17ea2b7f88c7
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2022-02-22 18:20:42 -0600

    DefaultProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import React, { Fragment } from 'react';
 
-const   PrimeraApp = ({ saludo }) => {
+const   PrimeraApp = ({ saludo, subtitulo }) => {
 
 
     return (
         <>
             <h1>{ saludo }</h1>;
             {/* <pre> { JSON.stringify( saludo, null, 3) }</pre> */}
-            <p>Mi primer aplicación</p>
+            <p> { subtitulo } </p>
         </>
     );
 }
@@ -18,5 +18,8 @@ PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired
 }
 
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
 
 export default PrimeraApp;
